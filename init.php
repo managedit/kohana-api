00a1a8292d99b95607e86b78a620c14181b9b33a
Kiall Mac Innes <kiall@managedit.ie>
2011-08-22 21:16:11 +0100

    Initial Commit. Needs work ;)

---
+++ init.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Extra HTTP Status Code Messages
+ */
+Response::$messages[102] = 'Processing';
+Response::$messages[207] = 'Multi-Status';
+Response::$messages[422] = 'Unprocessable Entity';
+Response::$messages[423] = 'Locked';
+Response::$messages[424] = 'Failed Dependency';
+Response::$messages[507] = 'Insufficient Storage';
+
+/**
+ * Routes
+ */
+
+Route::set('api', 'api/<controller>(/<id>)(/<custom>)', array('id' => '\d+'))
+	->defaults(array(
+		'directory'  => 'api',
+		'id'         => FALSE,
+		'action'     => 'index',
+	));
